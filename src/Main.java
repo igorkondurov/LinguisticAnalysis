b9a43b9647dbb762c3f4f8dcd7b601556f2f7a20
igorkondurov <igorkondurov@mail.ru>
2021-04-12 21:09:35 +0700

    12.04 Refactoring v2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import com.linguisticanalysis.scanner.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("123");
-
         Scanner scan = new Scanner("/Users/egor/Desktop/untitled.txt");
         Analyzer analyzer = new Analyzer(scan);
         analyzer.S();
+        System.out.println("Ошибок не обнаружено!");
     }
 }
